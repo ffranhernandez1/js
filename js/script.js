14742152e8b295cc23b19931dce0e06cde3f06a5
ffranhernandez101~ <franciscohernandezv03@gmail.com>
2022-12-01 12:57:19 -0300

    creo un sistema para calcular un promedio de edad

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-let pregunta=prompt("ingresa un numero");
-if(pregunta >= 10){
-    console.log("correcto");
+function CalculadorDeEdadPromedio(){
+    let resultado=(primerNum+segundoNum+tercerNum+cuartoNum)/4;
+    alert("el promedio de edad es de "+resultado)
 }
+
+let primerNum=parseInt(prompt("ingrese la primer edad"))
+let segundoNum=parseInt(prompt("ingrese la segunda edad"))
+let tercerNum=parseInt(prompt("ingrese la tercer edad"))
+let cuartoNum=parseInt(prompt("ingrese la cuarta edad"))
+
+CalculadorDeEdadPromedio()
